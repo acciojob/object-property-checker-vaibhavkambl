da73848afbfcaaa6d1bab258cabeb8418b1051b9
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-19 19:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	function hasKey(key) {
-  return key in sampleObject;
+	 return key in sampleObject;
 }
-	const sampleObject = {
-  red: "#FF0000",
-  green: "#00FF00",
-  white: "#FFFFFF"
-};
 	console.log(hasKey("red"));    // true
  console.log(hasKey("blue"));   // false
  console.log(hasKey("white"));  // true
-}
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
